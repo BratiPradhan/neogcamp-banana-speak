964d561dfca38130540decf6310dc97cb73cdac5
BratiPradhan <pradhanbrati@gmail.com>
2020-12-04 22:37:36 +0530

    feat: wired fetch call with error handling

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#txt-input');
-var outputDiv = document.querySelector('#output-div')
+var outputDiv = document.querySelector('#output-div');
+
+// TESTING URL
+// var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+// MINION TRANSLATOR URL
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+const getURL = text => encodeURI(url + "?" + "text=" + text)
+
 
 const clickHandler = () => {
+    // INPUT
     var inputValue = txtInput.value;
-    outputDiv.innerText = inputValue
+
+    // calling server for processing
+    fetch(getURL(inputValue))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+
+            // output
+            outputDiv.innerText = translatedText;
+        })
+        .catch(error => {
+            // error handling
+            alert('ERROR OCCURED FROM THE SERVER, TRY AGAIN AFTER SOME TIME' + error)
+        })
 }
 
-btnTranslate.addEventListener('click', clickHandler);   
+btnTranslate.addEventListener('click', clickHandler);
